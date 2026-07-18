31a2dfa4faa91bcaf1f343732d40288a3ec9ecea
Rafael Dutra <rafaeldutrasena@gmail.com>
2026-07-18 19:44:54 -0300

    fix: corrige classe container da seção de categorias

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -12,7 +12,7 @@ const categories = [
 ];
 
 export const Categories = () => {
-  return <section className="containe flex gap-2.5 lg:grid lg:grid-cols-4 lg:gap-4 mb-10 overflow-x-auto scrollbar-hide snap-x snap-mandatory">
+  return <section className="container flex gap-2.5 lg:grid lg:grid-cols-4 lg:gap-4 mb-10 overflow-x-auto scrollbar-hide snap-x snap-mandatory">
     {categories.map((category, index) => (
       <div key={index} style={{ backgroundImage: `url(${category.image})` }} className="h-125 bg-cover bg-center rounded-[20px] relative flex items-center justify-center text-white shrink-0 w-[95%] md:w-1/2 lg:w-full">
         <div className="absolute inset-0 bg-black/30 rounded-[20px] snap-center"></div>
