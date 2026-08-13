427e0e44f805f72f13c1035c692d33dc82217c17
Rafael Dutra <rafaeldutrasena@gmail.com>
2026-08-13 18:32:51 -0300

    feat: adiciona tela de nossas lojas

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,7 +8,7 @@ export const Header = () => {
 
     return (
         <div className="relative">
-            <header className="fixed top-10 left-0 right-0 z-10 mx-10 ">
+            <header className="fixed top-5 left-0 right-0 z-10 mx-10 ">
                 <div className="bg-white text-black max-w-330 mx-auto flex justify-between items-center py-3 px-7 rounded-2xl mt-5">
 
                     <Link to="/"><img src={Logo} alt="Logo SyntaxWear"
@@ -32,7 +32,7 @@ export const Header = () => {
                     <nav>
                         <ul className="flex gap-4 md:gap-10">
                             <li className="hidden md:block">
-                                <a href="">Nossas Lojas</a>
+                                <Link to="/our-stores">Nossas Lojas</Link>
                             </li>
                             <li className="hidden md:block">
                                 <a href="">Sobre</a>
